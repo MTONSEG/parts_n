108d629d1ecae3c9575b7b7898c7f9211ecb65c0
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-11-08 16:49:28 +0200

    update fetching data warranty page

--- src/app/(static_pages)/warranty/page.tsx
+++ src/app/(static_pages)/warranty/page.tsx
@@ -3,18 +3,43 @@ import { Breadcrumbs } from '@/components/common/Breadcrumbs/Breadcrumbs'
 import type { ILink } from '@/models/models'
 import Image from 'next/image'
 import './warranty.scss'
+import MarkDown from 'react-markdown'
+import { API } from '../../../api'
 
 export const metadata: Metadata = {
 	title: 'Warranties | NOUT PARTS',
 }
 
-export default function Warranty() {
+const getData = async (): Promise<WarrantyData> => {
+	const res = await fetch(
+		`${API}/warranties?populate[bonusIcon][populate]=*&populate[items]populate=*`,
+		{
+			next: {
+				revalidate: 7200
+			}
+		}
+	)
+
+	if (!res.ok) {
+		throw new Error('Failed to fetching data')
+	}
+
+	const data = await res.json()
+
+	return data.data[0].attributes
+}
+
+export default async function Warranty() {
 	const breadcrumbs: ILink[] = [
 		{
 			title: 'Гарантии',
 			path: '#',
 		},
 	]
+
+	const data: WarrantyData = await getData()
+
+	console.log(data)
 
 	return (
 		<>
@@ -23,145 +48,29 @@ export default function Warranty() {
 				<div className='container'>
 					<div className='warranty__row'>
 						<h1 className='warranty__title'>Гарантии</h1>
-						<div className='warranty__section'>
-							<h2 className='warranty__section-title'>
-								1. Общие положения
-							</h2>
-							<div className='warranty__section-body'>
-								<h3 className='warranty__section-subtitle'>
-									Компания Noutparts предоставляет гарантию на все
-									товары, продаваемые в нашем интернет магазине.
-								</h3>
-								<ul className='warranty__section-list'>
-									<li className='warranty__section-item'>
-										<span>1.1.</span> Все товары проходят
-										тестирование: при поступлении на наш склад и
-										непосредственно перед отправкой покупателю.
-									</li>
-									<li className='warranty__section-item'>
-										<span>1.2.</span> Любой товар, не подлежащий
-										тестированию, проходит процедуру тщательного
-										осмотра. .
-									</li>
-									<li className='warranty__section-item'>
-										<span>1.3.</span> На все группы
-										товаров распространяется гарантия от 3 месяцев до
-										12 месяцев со дня получения товара покупателем, на
-										разные группы товаров срок гарантии указан в
-										характеристике товара.
-									</li>
-									<li className='warranty__section-item'>
-										<span>1.4.</span> Продавец обязуется бесплатно
-										устранить дефекты товара посредством ремонта или
-										замены на аналогичный товар при условии, что
-										дефект возник по вине Продавца или является
-										производственным браком, в течение гарантийного
-										срока.
-									</li>
-									<li className='warranty__section-item'>
-										<span>1.5.</span> Интернет-магазин Unisfere не
-										берет на себя обязательств по замене гарантийного
-										товара на аналогичный на время ремонта или
-										диагностики, за исключением отдельных случаев
-										которые оговариваются в отдельном порядке с
-										уполномоченным представителем интернет-магазина.
-									</li>
-									<li className='warranty__section-item'>
-										<span>1.6.</span> Право на гарантийное
-										обслуживание имеет только непосредственный
-										покупатель товара.
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div className='warranty__section'>
-							<h2 className='warranty__section-title'>
-								2. Возврат и обмен
-							</h2>
-							<div className='warranty__section-body'>
-								<h3 className='warranty__section-subtitle warranty__section-subtitle_second'>
-									{`На все товары, купленные в нашем магазине,
-									распространяется действие Закона Украины "О защите
-									прав потребителей". Согласно статьи 9 Закона, товар
-									надлежащего качества (без дефектов) может быть
-									обменен или возвращен в магазин в течение 14 дней, не
-									считая дня покупки, если соблюдены следующие условия:`}
-								</h3>
-								<ul className='warranty__section-list'>
-									<li className='warranty__section-item warranty__section-item_disc'>
-										присутствуют все документы на товар (инструкции,
-										гарантийный талон, квитанция либо товарный чек,
-										подтверждающие покупку);
-									</li>
-									<li className='warranty__section-item  warranty__section-item_disc'>
-										товарный вид сохранен, то есть, остались все
-										ярлыки, упаковка, наклейки и прочие принадлежности
-										товара в неизменном виде;
-									</li>
-									<li className='warranty__section-item  warranty__section-item_disc'>
-										товаров, имеющих внешние дефекты (явные
-										механические или термические повреждения, трещины,
-										сломанные контактные группы и/или разъемы) любого
-										элемента товара;
-									</li>
-									<li className='warranty__section-item  warranty__section-item_disc'>
-										если количество битых (неработающих) пикселей
-										матрицы соответствует стандарту ISO 13406-2;
-									</li>
-									<li className='warranty__section-item  warranty__section-item_disc'>
-										товар не использовался.
-									</li>
-								</ul>
-								<p className='warranty__section-text'>
-									Мы ценим свое имя, надежного и качественного
-									поставщика качественных товаров, и уделяем повышенное
-									внимание любому Вашему обращению.
-								</p>
-							</div>
-						</div>
-						<div className='warranty__section'>
-							<h2 className='warranty__section-title'>
-								3. Процедура возврата средств
-							</h2>
-							<div className='warranty__section-body'>
-								<ul className='warranty__section-list'>
-									<li className='warranty__section-item  warranty__section-item_num'>
-										Установление факта распространения услуги на
-										данный товар.
-									</li>
-									<li className='warranty__section-item  warranty__section-item_num'>
-										Передача товара гарантийному отделу магазина, в
-										котором он был приобретен.
-									</li>
-									<li className='warranty__section-item  warranty__section-item_num'>
-										Сотрудники отдела производят внешний осмотр, а в
-										случаях когда это необходимо – проверку
-										работоспособности. После этого они принимают
-										решение о возможности возврата средств.
-									</li>
-									<li className='warranty__section-item  warranty__section-item_num'>
-										Оформление возврата денег в месте покупки товара.
-										Деньги могут быть возвращены в рабочий день (с
-										понедельника по пятницу) с 09:00 до 18:00.
-									</li>
-								</ul>
-							</div>
-						</div>
+						<ul className='warranty__list'>
+							{data.items.map(el => (
+								<li className='warranty__item' key={el.id}>
+									<h2 className='warranty__item-title'>{el.title}</h2>
+									<div className='warranty__item-body'>
+										<MarkDown>{el.text}</MarkDown>
+									</div>
+								</li>
+							))}
+						</ul>
+
 						<div className='warranty__bonus'>
 							<Image
-								src='/icons/bonus.svg'
+								src={data.bonusIcon.data[0].attributes.url}
 								width={78}
 								height={88}
 								alt='bonus'
 								className='warranty__icon'
 							/>
 							<div className='warranty__text-wrap'>
-								<h3>Бонус для постоянных клиентов</h3>
+								<h3>{data.titleBonus}</h3>
 								<p>
-									Постоянные клиенты могут рассчитывать на упрощенную
-									систему по обмену бракованного товара. Благодаря
-									этому проблема решается в самые сжатые сроки с
-									минимумом издержек.
+									{data.textBonus}
 								</p>
 							</div>
 							<Image
@@ -185,4 +94,66 @@ export default function Warranty() {
 			</div>
 		</>
 	)
+}
+
+interface WarrantyData {
+	title: string
+	createdAt: string
+	updatedAt: string
+	publishedAt: string
+	titleBonus: string
+	textBonus: string
+	bonusIcon: BonusIcon
+	items: ItemWarranty[]
+}
+
+interface ItemWarranty {
+	id: number
+	title: string
+	text: string
+}
+
+export interface BonusIcon {
+	data: Daum[]
+}
+
+export interface Daum {
+	id: number
+	attributes: Attributes
+}
+
+export interface Attributes {
+	name: string
+	alternativeText: any
+	caption: any
+	width: number
+	height: number
+	formats: any
+	hash: string
+	ext: string
+	mime: string
+	size: number
+	url: string
+	previewUrl: any
+	provider: string
+	provider_metadata: ProviderMetadata
+	createdAt: string
+	updatedAt: string
+	related: Related[]
+}
+
+export interface ProviderMetadata {
+	public_id: string
+	resource_type: string
+}
+
+export interface Related {
+	__type: string
+	id: number
+	title: string
+	createdAt: string
+	updatedAt: string
+	publishedAt: string
+	titleBonus: string
+	textBonus: string
 }
